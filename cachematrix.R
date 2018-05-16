1e4c42fc9929d0fbf4a0541b0b89a7be748e2ef0
Percy5d7 <percy5d7glen@yahoo.com>
2018-05-17 00:06:33 +0800

    thanks

--- cachematrix.R
+++ cachematrix.R
@@ -2,36 +2,47 @@
 ## functions do
 
 ## Write a short comment describing this function
-makeCacheMatrix <- function(x = matrix(c(1:9), nrow =3, ncol = 3)) {
+
+makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  
   get <- function() x
-  setInverse <- function(inverse) inv <<- inverse
+  setInverse <- function() inv <<- solve(x) #calculate the inverse
   getInverse <- function() inv
-  
   list(set = set,
        get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
+#Example:
+funs <- makeCacheMatrix()
+funs$set(matrix(1:4, 2))
+funs$get()
+funs$setInverse()
+funs$getInverse()
 
-cacheSolve <- function(makeCacheMatrix , ...) {
+## Write a short comment describing this function
+# The point is that x and inv are stored in the enclosing 
+# environment of the set, get, setInverse, getInverse 
+# functions. That means the environment within which 
+# they were defined, i.e., the environment created by 
+# makeCacheMatrix()
+
+
+
+cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  
-  inv <- x$getInverse()
-  
-  if (!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
+  inv_x <- x$getinverse()
+  if (!is.null(inv_x)) {
+    message("getting cached inverse matrix")
+    return(inv_x)
+  } else {
+    inv_x <- solve(x$get())
+    x$setinverse(inv_x)
+    return(inv_x)
   }
-  
-  mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setInverse(inv)
-  inv
 }
